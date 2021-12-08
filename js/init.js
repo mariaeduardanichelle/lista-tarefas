29b1b0627bd87120feb51ff8803a6cb7f573a585
mariaeduardanichelle <95757504+mariaeduardanichelle@users.noreply.github.com>
2021-12-08 10:43:40 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAfKpFrqJ-LbNbkzr7X1NEkSiuyxCiySNk",
+    authDomain: "todo-list-8d5e4.firebaseapp.com",
+    projectId: "todo-list-8d5e4",
+    storageBucket: "todo-list-8d5e4.appspot.com",
+    messagingSenderId: "476657434019",
+    appId: "1:476657434019:web:a9d507755dda46b5d82e16",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
